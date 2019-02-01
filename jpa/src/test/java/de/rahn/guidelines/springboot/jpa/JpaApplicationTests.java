e306ef4b95c332b2b25835ec1d3bbd7f907cb49b
Frank W. Rahn <frank@frank-rahn.de>
2019-02-01 22:56:55 +0100

    Import der Generierung

---
+++ jpa/src/test/java/de/rahn/guidelines/springboot/jpa/JpaApplicationTests.java
@@ -0,0 +1,17 @@
+package de.rahn.guidelines.springboot.jpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
