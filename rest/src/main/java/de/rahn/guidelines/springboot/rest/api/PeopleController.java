107151c1051dc7fd9510f7b661c4035fdece9a6e
Frank W. Rahn <frank@frank-rahn.de>
2019-02-10 13:10:46 +0100

    Die restlichen Methoden implementiert.

--- rest/src/main/java/de/rahn/guidelines/springboot/rest/api/PeopleController.java
+++ rest/src/main/java/de/rahn/guidelines/springboot/rest/api/PeopleController.java
@@ -2,6 +2,7 @@ package de.rahn.guidelines.springboot.rest.api;
 
 import de.rahn.guidelines.springboot.rest.domain.people.Person;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
@@ -13,37 +14,60 @@ import java.util.*;
 @RequestMapping(path = "/api/people")
 public class PeopleController {
 	
-	private List<Person> people = new ArrayList<>();
+	private Map<String, Person> people = new HashMap<>();
+	
+	private Person addPersonToPeople(Person person) {
+		Assert.notNull(person.getId(), "Id darf nicht null sein!");
+		
+		people.put(person.getId(), person);
+		
+		return person;
+	}
+	
+	private Person addPersonToPeople(String id, Person person) {
+		if (id != null) {
+			person.setId(id);
+		} else {
+			person.setId(UUID.randomUUID().toString());
+		}
+		
+		return addPersonToPeople(person);
+	}
 	
 	@PostConstruct
 	protected void setup() {
-		Person person=new Person();
+		Person person = new Person();
 		person.setId(UUID.randomUUID().toString());
 		person.setFirstName("Frank");
 		person.setLastName("Rahn");
-		person.setBirthday(LocalDate.of(1967, 5,5));
-		people.add(person);
+		person.setBirthday(LocalDate.of(1967, 5, 5));
+		
+		addPersonToPeople(person);
 	}
 	
 	@GetMapping
-	public List<Person> getAllPeople() {
-		return people;
+	public Collection<Person> getAllPeople() {
+		return people.values();
 	}
 	
 	@GetMapping(path = "/{id}")
 	public ResponseEntity<Person> getPersonById(@PathVariable("id") String id) {
-		
-		return ResponseEntity.of(
-				people.stream().filter(person -> person.getId().equals(id)).findFirst()
-		);
+		return ResponseEntity.of(Optional.ofNullable(people.get(id)));
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<Person> removePersonById(@PathVariable("id") String id) {
+		return ResponseEntity.of(Optional.ofNullable(people.remove(id)));
 	}
 	
 	@PostMapping
 	public Person newPerson(@RequestBody @Valid Person person) {
-		person.setId(UUID.randomUUID().toString());
-		people.add(person);
-		
-		return person;
+		return addPersonToPeople(null, person);
+	}
+	
+	@PutMapping(path = "/{id}")
+	public Person newPerson(@PathVariable("id") String id, @RequestBody @Valid Person person) {
+		return addPersonToPeople(id, person);
 	}
 	
 }
