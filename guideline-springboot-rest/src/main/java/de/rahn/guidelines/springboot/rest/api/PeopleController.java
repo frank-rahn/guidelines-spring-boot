3511c3d16d8585506e256fe931f3ad7b3b0e76a6
Frank W. Rahn <frank@frank-rahn.de>
2019-04-10 22:13:09 +0200

    JSON mit Charset UTF-8

--- guideline-springboot-rest/src/main/java/de/rahn/guidelines/springboot/rest/api/PeopleController.java
+++ guideline-springboot-rest/src/main/java/de/rahn/guidelines/springboot/rest/api/PeopleController.java
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api/people", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "/api/people", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 @Api(tags = {"People"})
 public class PeopleController {
 
