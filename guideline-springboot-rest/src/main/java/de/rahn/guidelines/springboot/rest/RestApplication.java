ba7fec9ef97d4f744de3575e21afbe1191f8b3de
Frank W. Rahn <frank@frank-rahn.de>
2019-04-22 22:52:28 +0200

    Main Klasse wieder public gemacht

--- guideline-springboot-rest/src/main/java/de/rahn/guidelines/springboot/rest/RestApplication.java
+++ guideline-springboot-rest/src/main/java/de/rahn/guidelines/springboot/rest/RestApplication.java
@@ -20,7 +20,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-class RestApplication {
+public class RestApplication {
   public static void main(String[] args) {
     SpringApplication.run(RestApplication.class, args);
   }
