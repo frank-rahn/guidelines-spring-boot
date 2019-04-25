1ad54754c6b56beae79cb291b53c2fdd00b8b792
Frank W. Rahn <frank@frank-rahn.de>
2019-04-25 07:54:32 +0200

    Formatiere Copyright Kommentar

--- guideline-springboot-rest/src/test/java/de/rahn/guidelines/springboot/rest/RestApplicationTests.java
+++ guideline-springboot-rest/src/test/java/de/rahn/guidelines/springboot/rest/RestApplicationTests.java
@@ -1,4 +1,4 @@
-/***************************************************************************************************
+/*
  * Copyright (c) 2019-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,8 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- **************************************************************************************************/
-
+ */
 package de.rahn.guidelines.springboot.rest;
 
 import org.junit.jupiter.api.Test;
