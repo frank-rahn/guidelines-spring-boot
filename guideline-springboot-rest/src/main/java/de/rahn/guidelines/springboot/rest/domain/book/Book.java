77371298514e6bd64cea10a93aa785f878d08963
Frank W. Rahn <frank@frank-rahn.de>
2019-11-06 20:00:20 +0100

    Spring Data Rest entfernt

---
+++ guideline-springboot-rest/src/main/java/de/rahn/guidelines/springboot/rest/domain/book/Book.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2019-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.rahn.guidelines.springboot.rest.domain.book;
+
+import static org.hibernate.validator.constraints.ISBN.Type.ISBN_10;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.hibernate.validator.constraints.ISBN;
+
+@ApiModel(description = "Ein Buch")
+@XmlRootElement
+public class Book {
+
+  @ApiModelProperty(value = "Die Artikelnummer", required = true, example = "4711")
+  @NotBlank
+  private final String id;
+
+  @ApiModelProperty(value = "Die zehnstellige ISBN", required = true, example = "0123456789")
+  @ISBN(type = ISBN_10)
+  private final String isbn;
+
+  @ApiModelProperty("Der Titel des Buches sollte mindestens 5 Zeichen umfassen")
+  @Size(min = 5)
+  private String title;
+
+  public Book(final String id, final String isbn, String title) {
+    this.id = id;
+    this.isbn = isbn;
+    this.title = title;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getIsbn() {
+    return isbn;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+}
