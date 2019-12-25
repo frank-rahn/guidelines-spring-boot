487465f6539cefbf136cf13b0b0eac3ad209fa6f
Frank W. Rahn <frank@frank-rahn.de>
2019-12-25 21:12:45 +0100

    Restliche Test für den ReportHelper fertig gestellt

--- guideline-springboot-batch/src/test/java/de/rahn/guidelines/springboot/batch/report/support/ReportHelperTest.java
+++ guideline-springboot-batch/src/test/java/de/rahn/guidelines/springboot/batch/report/support/ReportHelperTest.java
@@ -15,8 +15,13 @@
  */
 package de.rahn.guidelines.springboot.batch.report.support;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import ch.qos.logback.classic.Level;
@@ -24,6 +29,7 @@ import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.Appender;
 import de.rahn.guidelines.springboot.batch.report.ReportJobExecutionListener;
+import java.sql.SQLException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -31,12 +37,16 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 
 @ExtendWith(MockitoExtension.class)
 class ReportHelperTest {
 
   @Mock
   private Appender<ILoggingEvent> mockAppender;
+
+  @Mock
+  private org.slf4j.Logger mockLogger;
 
   @Spy
   private ReportHelper classUnderTest = new ReportHelper();
@@ -102,5 +112,113 @@ class ReportHelperTest {
                   assertThat(argument.getLevel()).isEqualTo(Level.ERROR);
                   return true;
                 }));
+  }
+
+  @Test
+  void givenMessageAndRuntimeExceptionWithoutItem_whenReportThrowable_thenLoggingMessageAsError() {
+    // Given
+    String message = "test";
+    Throwable exception = new RuntimeException(message);
+
+    doReturn(false).when(mockLogger).isErrorEnabled();
+
+    // When
+    classUnderTest.reportThrowable(message, null, exception, mockLogger);
+
+    // Then
+    verify(mockAppender)
+        .doAppend(
+            argThat(
+                argument -> {
+                  String msg = argument.getFormattedMessage();
+                  assertThat(msg).startsWith(message);
+                  assertThat(msg).endsWith(message);
+                  assertThat(argument.getLevel()).isEqualTo(Level.ERROR);
+                  return true;
+                }));
+    verify(mockLogger, times(1)).isErrorEnabled();
+  }
+
+  @Test
+  void
+  givenMessageAndRuntimeExceptionAndItem_whenReportThrowable_thenLoggingMessageAsErrorAndException() {
+    // Given
+    String message = "test";
+    Throwable exception = new RuntimeException(message);
+    Object item = asList("test-a", "test-b");
+
+    doReturn(true).when(mockLogger).isErrorEnabled();
+
+    // When
+    classUnderTest.reportThrowable(message, item, exception, mockLogger);
+
+    // Then
+    verify(mockAppender)
+        .doAppend(
+            argThat(
+                argument -> {
+                  String msg = argument.getFormattedMessage();
+                  assertThat(msg).startsWith(message);
+                  assertThat(msg).endsWith(message);
+                  assertThat(argument.getLevel()).isEqualTo(Level.ERROR);
+                  return true;
+                }));
+    verify(mockLogger, times(1)).isErrorEnabled();
+  }
+
+  @Test
+  void
+  givenMessageAndDataAccessExceptionAndItemAndSQLException_whenReportThrowable_thenLoggingMessageAsErrorAndException() {
+    // Given
+    String message = "test";
+    Throwable exception = new DataAccessException(message) {
+    };
+    Object item = asList("test-a", "test-b");
+
+    doReturn(true).when(mockLogger).isErrorEnabled();
+
+    // When
+    classUnderTest.reportThrowable(message, item, exception, mockLogger);
+
+    // Then
+    verify(mockAppender)
+        .doAppend(
+            argThat(
+                argument -> {
+                  String msg = argument.getFormattedMessage();
+                  assertThat(msg).startsWith(message);
+                  assertThat(msg).endsWith(message);
+                  assertThat(argument.getLevel()).isEqualTo(Level.ERROR);
+                  return true;
+                }));
+    verify(mockLogger, times(1)).isErrorEnabled();
+    verify(mockLogger, times(1)).error(anyString(), any(Throwable.class));
+  }
+
+  @Test
+  void
+  givenMessageAndDataAccessExceptionAndSQLExceptionAndItem_whenReportThrowable_thenLoggingMessageAsErrorAndException() {
+    // Given
+    String message = "test";
+    Throwable exception =
+        new DataAccessException(message, new SQLException("reason", "SQLState", 4711)) {
+        };
+    Object item = asList("test-a", "test-b");
+
+    // When
+    classUnderTest.reportThrowable(message, item, exception, mockLogger);
+
+    // Then
+    verify(mockAppender)
+        .doAppend(
+            argThat(
+                argument -> {
+                  String msg = argument.getFormattedMessage();
+                  assertThat(msg).startsWith(message);
+                  assertThat(argument.getLevel()).isEqualTo(Level.ERROR);
+                  return true;
+                }));
+    verify(mockLogger, times(0)).isErrorEnabled();
+    verify(mockLogger, times(1)).error(anyString(), any(Throwable.class));
   }
 }
